2323913840415f6accd9f93b8ac7a12582357fe6
hey-oise <oise.giwa.okugbe@gmail.com>
2026-08-03 17:10:08 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,28 @@ import { useState } from "react";
 export default function Home() {
   const [query, setQuery] = useState("");
   const products = [
-    { id: 1, name: "Indomie Regular", extra: "Super Pack (Single)", imageLink: "/product/indomie.jpg" },
-    { id: 2, name: "Milo Chocolate Malt", extra: "400g Tin", imageLink: "/product/milo.jpg" },
-    { id: 3, name: "Peak Evaporated Milk", extra: "150g Can", imageLink: "/product/peak.jpg" },
-    { id: 4, name: "Golden Penny Spaghetti", extra: "500g Pack", imageLink: "/product/spaghetti.jpg" },
-    { id: 5, name: "Garri", extra: "yello garri", imageLink: "/product/garri.jpg" },
-    { id: 6, name: "Dangote Sugar", extra: "500g Granulated Pack", imageLink: "/product/sugar.jpg" },
-    { id: 7, name: "Kings Vegetable Oil", extra: "1 Litre Bottle", imageLink: "/product/oil.jpg" },
-    { id: 8, name: "Maggi Star Cubes", extra: "Seasoning (100pcs)", imageLink: "/product/maggi.jpg" },
-    { id: 9, name: "St. Louis Sugar", extra: "500g Cube Box", imageLink: "/product/sugar.jpg" },
-    { id: 10, name: "Dano Full Cream Milk", extra: "400g Pouch Bag", imageLink: "/product/peak.jpg" },
-    { id: 11, name: "Honeywell Semolina", extra: "1kg Pack", imageLink: "/product/garri.jpg" },
-    { id: 12, name: "Gino Tomato Paste", extra: "70g Sachet", imageLink: "/product/milo.jpg" },
-    { id: 13, name: "Knorr Beef Cubes", extra: "Seasoning (50pcs)", imageLink: "/product/maggi.jpg" },
-    { id: 14, name: "Sardine in Vegetable Oil", extra: "Titan Brand Can", imageLink: "/product/oil.jpg" },
-    { id: 15, name: "Cabin Biscuits", extra: "Oxford Standard Pack", imageLink: "/product/spaghetti.jpg" },
-    { id: 16, name: "Power Oil", extra: "750ml Clean Bottle", imageLink: "/product/oil.jpg" },
-    { id: 17, name: "Bigi Cola", extra: "50cl Plastic Bottle", imageLink: "/product/milo.jpg" },
-    { id: 18, name: "Malta Guinness", extra: "330ml Classic Can", imageLink: "/product/peak.jpg" },
-    { id: 19, name: "Nascom Cornflakes", extra: "350g Box Pack", imageLink: "/product/indomie.jpg" },
-    { id: 20, name: "Golden Morn", extra: "Cereal Maize 450g", imageLink: "/product/milo.jpg" }
+    { id: 1, name: "Indomie superpack", price: "₦400", imageLink: "/product/indomie.jpg" },
+    { id: 2, name: "Milo Chocolate Malt", price: "₦250", imageLink: "/product/milo.jpg" },
+    { id: 3, name: "Peak Evaporated Milk", price: "₦300", imageLink: "/product/peak.jpg" },
+    { id: 4, name: "Golden Penny Spaghetti", price: "₦700", imageLink: "/product/spaghetti.jpg" },
+    { id: 5, name: "Garri", price: "₦500", imageLink: "/product/garri.jpg" },
+    { id: 6, name: "Dangote Sugar", price: "₦1,200", imageLink: "/product/sugar.jpg" },
+    { id: 7, name: "Kings Vegetable Oil", price: "₦5,000", imageLink: "/product/oil.jpg" },
+    { id: 8, name: "Maggi Star Cubes", price: "₦50", imageLink: "/product/maggi.jpg" },
+    { id: 9, name: "St. Louis Sugar", price: "₦3,000", imageLink: "/product/sugar.jpg" },
+    { id: 10, name: "Dano Full Cream Milk", price: "400g Pouch Bag", imageLink: "/product/peak.jpg" },
+    { id: 11, name: "Honeywell Semolina", price: "1kg Pack", imageLink: "/product/garri.jpg" },
+    { id: 12, name: "Gino Tomato Paste", price: "70g Sachet", imageLink: "/product/milo.jpg" },
+    { id: 13, name: "Knorr Beef Cubes", price: "Seasoning (50pcs)", imageLink: "/product/maggi.jpg" },
+    { id: 14, name: "Sardine in Vegetable Oil", price: "Titan Brand Can", imageLink: "/product/oil.jpg" },
+    { id: 15, name: "Cabin Biscuits", price: "Oxford Standard Pack", imageLink: "/product/spaghetti.jpg" },
+    { id: 16, name: "Power Oil", price: "750ml Clean Bottle", imageLink: "/product/oil.jpg" },
+    { id: 17, name: "Bigi Cola", price: "50cl Plastic Bottle", imageLink: "/product/milo.jpg" },
+    { id: 18, name: "Malta Guinness", price: "330ml Classic Can", imageLink: "/product/peak.jpg" },
+    { id: 19, name: "Nascom Cornflakes", price: "350g Box Pack", imageLink: "/product/indomie.jpg" },
+    { id: 20, name: "Golden Morn", price: "Cereal Maize 450g", imageLink: "/product/milo.jpg" }
   ];
-  const filteredProduct = products.filter(item => item.name.toLowerCase().includes(query.toLowerCase()) || item.extra.toLowerCase().includes(query.toLowerCase()));
+  const filteredProduct = products.filter(item => item.name.toLowerCase().includes(query.toLowerCase()) || item.price.toLowerCase().includes(query.toLowerCase()));
 
   return (
     <div className=" bg-amber-100 pt-70 min-w-full min-h-full flex-1">
@@ -46,9 +46,9 @@ export default function Home() {
         }
       </div>
         {filteredProduct.length > 0 ?
-        <div className="m-auto grid md:grid-cols-4 grid-cols-2 lg:grid-cols-7 sm:grid-cols-3 w-fit gap-5 sm:gap-10 mt-60 mb-40">
+        <div className="m-auto grid md:grid-cols-4 grid-cols-2 lg:grid-cols-7 sm:grid-cols-3 w-fit gap-5 sm:gap-7 mt-60 mb-40">
           {filteredProduct.map((item) => {
-            return <Product key={item.id} name={item.name} extra={item.extra} imageLink={item.imageLink} />
+            return <Product key={item.id} name={item.name} price={item.price} imageLink={item.imageLink} />
           } )}
       </div>
         : <p className="text-amber-600 flex gap-1 font-bold text-md m-auto capitalize items-center justify-center mt-50"><SearchAlert/> no item found!</p>
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,20 +1,21 @@
 import { ShoppingBasketIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Product({name = "", extra ="", imageLink = ""}) {
+export default function Product({name = "", price ="", imageLink = ""}) {
   return (
-    <div className="flex flex-row gap-5 m-auto hover:scjale-101 transition-all duration-500">
-      <div className="w-36 h-60 rounded-lg overflow-hidden border border-amber-200 relative">
+    <Link target="_blank" href={`https://wa.me/2349077453404?text=hi, i need one ${name + " " + price}`} className="flex flex-row gap-5 m-auto hover:scjale-101 transition-all duration-500">
+      <div className="w-36 h-60 rounded-2xl overflow-hidden border border-amber-700 relative">
         <button className="absolute top-1 right-1 text-emerald-500-500 bg-amber-100/70 p-1 rounded-lg"><ShoppingBasketIcon/></button>
                 <Image src={imageLink} alt={`image of ${name}`} width={700} height={1000} className="w-full h-full z-0 object-cover pointer-events-none select-none" draggable={false}></Image>
-        <div className="absolute bottom-0 p-3 z-10 bg-linear-to-t from-gray-500 to-transparent w-full min-h-20 flex flex-col justify-center">
+        <div className="absolute bottom-0 p-3 z-10 bg-linear-to-t from-black to-transparent w-full min-h-20 flex flex-col justify-center">
           <div>
-          <p className="text-gray-900 capitalize font-bold text-md">{name}
+          <p className="text-gray-200 capitalize font-bold text-md">{name}
           </p>
-            <p className="text-amber-200 capitalize text-sm font-semibold">{extra}</p>
+            <p className="text-green-100 capitalize text-sm font-semibold">{price}</p>
           </div>
                 </div>
               </div>
-              </div>
+              </Link>
   )
 }
