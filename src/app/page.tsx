042cc4bcd76733741d091b23f1e3800b07817b2f
hey-oise <oise.giwa.okugbe@gmail.com>
2026-08-03 18:50:15 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,20 +33,20 @@ export default function Home() {
   const filteredProduct = products.filter(item => item.name.toLowerCase().includes(query.toLowerCase()) || item.price.toLowerCase().includes(query.toLowerCase()));
 
   return (
-    <div className=" bg-amber-100 pt-70 min-w-full min-h-full flex-1">
-      <div className="fixed top-0 left-0 right-0 border-b border-amber-300 h-16 flex items-center px-4 justify-between backdrop-blur-2xl z-30">
+    <div className=" bg-amber-100 pt-50 min-w-full min-h-full flex-1">
+      <div className="fixed top-0 left-0 right-0 bordder-b border-amber-300 h-16 flex items-center px-4 justify-between backdrop-blur-2xl z-30">
         <Link href={"/"} className="text-amber-400 capitalize font-bold flex gap-1 text-md items-center">
           <StoreIcon size={20}/>
           konaMarket</Link>
         <Link href={"/"} className="hover:bg-amber-200 transition-all duration-400 p-2 rounded-full"><ShoppingCartIcon size={20} className="text-amber-400"/></Link>
       </div>
-      <div className="bg-amber-200 w-4/5 lg:w-1/2 h-10 flex items-center px-2 rounded-lg border border-amber-400 gap-3 m-auto"><SearchIcon className="text-amber-500" /><input onChange={(e) => setQuery(e.target.value)} value={query} type="search" className="w-full outline-none text-amber-600 placeholder-amber-500" placeholder="what are you looking for?"></input>
+      <div className="bg-amber-200 w-4/5 lg:w-1/2 h-10 flex items-center px-2 rounded-lg border border-amber-200 gap-3 m-auto"><SearchIcon className="text-amber-500" /><input onChange={(e) => setQuery(e.target.value)} value={query} type="search" className="w-full outline-none text-amber-600 placeholder-amber-500" placeholder="what are you looking for?"></input>
         {query.length > 0 &&
           <button onClick={() => setQuery("")}><XIcon className="text-amber-500" /></button>
         }
       </div>
         {filteredProduct.length > 0 ?
-        <div className="m-auto grid md:grid-cols-4 grid-cols-2 lg:grid-cols-7 sm:grid-cols-3 w-fit gap-5 sm:gap-7 mt-60 mb-40">
+        <div className="m-auto grid md:grid-cols-4 grid-cols-2 lg:grid-cols-7 sm:grid-cols-3 w-fit gap-5 sm:gap-7 mt-40 mb-40">
           {filteredProduct.map((item) => {
             return <Product key={item.id} name={item.name} price={item.price} imageLink={item.imageLink} />
           } )}
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function Product({name = "", price ="", imageLink = ""}) {
   return (
-    <Link target="_blank" href={`https://wa.me/2349077453404?text=hi, i need one ${name + " " + price}`} className="flex flex-row gap-5 m-auto hover:scjale-101 transition-all duration-500">
+    <Link target="_blank" href={`https://wa.me/2349077453404?text=hi, i need one ${name}`} className="flex flex-row gap-5 m-auto hover:scjale-101 transition-all duration-500">
       <div className="w-36 h-60 rounded-2xl overflow-hidden border border-amber-700 relative">
         <button className="absolute top-1 right-1 text-emerald-500-500 bg-amber-100/70 p-1 rounded-lg"><ShoppingBasketIcon/></button>
                 <Image src={imageLink} alt={`image of ${name}`} width={700} height={1000} className="w-full h-full z-0 object-cover pointer-events-none select-none" draggable={false}></Image>
         <div className="absolute bottom-0 p-3 z-10 bg-linear-to-t from-black to-transparent w-full min-h-20 flex flex-col justify-center">
-          <div>
+          <div className="w-full">
           <p className="text-gray-200 capitalize font-bold text-md">{name}
           </p>
             <p className="text-green-100 capitalize text-sm font-semibold">{price}</p>
